b7beda3231db560c00c8f76386c3013c73cca04d
viccyviccy <iamviccyviccy@gmail.com>
2019-09-11 17:20:25 +0300

    Updates

---
+++ precache-manifest.42feeed8f45c57d1e1ebbd4f51bfc9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88aa52ac12788968827c06be8f515e8",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "7f1ce777ea6fea7629da",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a49a394b.chunk.css"
+  },
+  {
+    "revision": "071f0a74c0a3f27a524c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.2c589504.chunk.js"
+  },
+  {
+    "revision": "7f1ce777ea6fea7629da",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.75d49a47.chunk.js"
+  },
+  {
+    "revision": "64eb9730415c0607730d",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.303cb74d.js"
+  }
+]);
